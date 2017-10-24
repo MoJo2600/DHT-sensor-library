22787ef5cda55a24b7875277600e9be1e2b73394
Christian Erhardt <mail@mojo2k.de>
2017-10-24 21:15:48 +0200

    Update DHT.cpp

--- DHT.cpp
+++ DHT.cpp
@@ -138,12 +138,12 @@ boolean DHT::read(bool force) {
   // Go into high impedence state to let pull-up raise data line level and
   // start the reading process.
   digitalWrite(_pin, HIGH);
-  delay(250);
+  //delay(250);
 
   // First set data line low for 20 milliseconds.
   pinMode(_pin, OUTPUT);
   digitalWrite(_pin, LOW);
-  delay(20);
+  //delay(20);
 
   uint32_t cycles[80];
   {
